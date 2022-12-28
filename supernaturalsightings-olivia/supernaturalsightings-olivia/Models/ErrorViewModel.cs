67378f63a72c9f905667ee9d6ae87ef562205717
codeswitchingstl <codeswitching@icloud.com>
2022-12-27 19:52:19 -0600

    initial commit

---
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace supernaturalsightings_olivia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
