6d95470252b76a5e621a9696782295326dbcb67c
codeswitchingstl <codeswitching@icloud.com>
2023-01-07 16:10:04 -0600

    initial commit

--- supernaturalsightings-olivia/supernaturalsightings-olivia/Controllers/AccountController.cs
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using supernaturalsightings_olivia.ViewModels;
 
@@ -57,6 +58,39 @@ namespace supernaturalsightings_olivia.Controllers
             }
             return View(model);
 
+        }
+
+        // Creates login action
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user.Email, user.Password, user.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Login");
         }
     }
 }
---
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+namespace supernaturalsightings_olivia.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        // Remember me feature
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
