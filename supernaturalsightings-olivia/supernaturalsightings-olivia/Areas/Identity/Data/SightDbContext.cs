2da53308e96bfa527b32e26eab0a9418b8d88f64
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2023-01-27 21:25:41 -0600

    SightDbContext.cs

--- supernaturalsightings-olivia/supernaturalsightings-olivia/Areas/Identity/Data/SightDbContext.cs
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/Areas/Identity/Data/SightDbContext.cs
@@ -18,4 +18,5 @@ public class SightDbContext : IdentityDbContext<IdentityUser, IdentityRole, stri
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
     }
+
 }
