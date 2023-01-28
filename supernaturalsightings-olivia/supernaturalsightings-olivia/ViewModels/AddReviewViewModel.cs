5f8c8505f819aced89b01bdcd7915922ac6640ca
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2023-01-27 21:24:09 -0600

    AddReviewViewModel.cs done

---
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/ViewModels/AddReviewViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace supernaturalsightings_olivia.ViewModels
+{
+    public class AddReviewViewModel
+    {
+
+
+        [Required(ErrorMessage = "Username is required!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 15 characters long")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Tell us your weird experience!")]
+        [StringLength(1000, MinimumLength = 3, ErrorMessage = "Review Comment must be longer than that")]
+        public string ReviewComment { get; set; }
+
+        [Required(ErrorMessage = "")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Pick a category")]
+        [Display(Name = "ReviewTitle")]
+        public string ReviewTitle { get; set; }
+
+
+        public AddReviewViewModel()
+        {
+        }
+   
+        }
+        
+    }
+
