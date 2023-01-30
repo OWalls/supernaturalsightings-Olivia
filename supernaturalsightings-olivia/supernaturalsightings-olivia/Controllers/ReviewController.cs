05bc60c4f302e443d7ab9f828a80d0da6b7f13c6
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2023-01-30 12:52:25 -0600

    ReAdded ReviewController

---
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/Controllers/ReviewController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;  // [Route] , ControllerBase
+using supernaturalsightings_olivia.Areas.Identity.Data;
+using supernaturalsightings_olivia.Models;
+using supernaturalsightings_olivia.ViewModels;
+
+namespace supernaturalsightings_olivia.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly SightDbContext _context;
+        public ReviewController(SightDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get: /<controller>/
+        public IActionResult Index()
+        {
+            List<Review> reviews = _context.Reviews
+                .ToList();
+            return View(reviews);
+        }
+
+
+        //  [HttpGet("/Review")]
+        public IActionResult AddReview()
+        {
+            AddReviewViewModel viewModel = new AddReviewViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        // [Route("/Review/Add")]
+        //[ValidateAntiForgeryToken]
+        public IActionResult ProcessAddReviewForm(AddReviewViewModel addReviewViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Review newReview = new Review
+                {
+                    Username = addReviewViewModel.Username,
+                    ReviewComment = addReviewViewModel.ReviewComment,
+                    ReviewTitle = addReviewViewModel.ReviewTitle,
+                };
+
+                _context.Add(newReview);
+                _context.SaveChanges();
+
+                return Redirect("/Review");
+            }
+
+            return View("AddReview", addReviewViewModel);
+        }
+
+    }
+}
