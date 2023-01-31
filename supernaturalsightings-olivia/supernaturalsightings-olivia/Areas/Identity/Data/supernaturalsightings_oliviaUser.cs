b50e7c690622ad05b40f169575c5ae625d356eb4
Tanya Buechel <tanyabuechel@gmail.com>
2023-01-30 21:49:54 -0600

    Scaffolded IdentityUser class into project.

---
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/Areas/Identity/Data/supernaturalsightings_oliviaUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace supernaturalsightings_olivia.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the supernaturalsightings_oliviaUser class
+public class supernaturalsightings_oliviaUser : IdentityUser
+{
+}
+
