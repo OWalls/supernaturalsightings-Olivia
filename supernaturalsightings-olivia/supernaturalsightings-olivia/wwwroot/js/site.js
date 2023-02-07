c95048cd660807623239e795faabf176623ab850
Tanya Buechel <tanyabuechel@gmail.com>
2023-02-07 16:41:27 -0600

    Collapsible descriptions on Results page.

--- supernaturalsightings-olivia/supernaturalsightings-olivia/wwwroot/js/site.js
+++ supernaturalsightings-olivia/supernaturalsightings-olivia/wwwroot/js/site.js
@@ -13,3 +13,21 @@ function changeAddFavoritesButton(id) {
     document.getElementById("filter_btn_" + id).style.backgroundColor = 'dodgerblue';
     document.getElementById("filter_btn_" + id).style.color = 'white';
 }
+
+
+//Results Collapse Function
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i <= coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+          content.style.display = "none";
+    } else {
+    content.style.display = "block";
+    }
+    });
+}
+                            
